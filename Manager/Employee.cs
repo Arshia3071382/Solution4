74fd932ececbe125685ab5a066f2c1275b7d0a0a
Asus <Asus@ARSHIA>
2023-10-17 00:10:34 +0330

    Add project files.

---
+++ Manager/Employee.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager;
+
+public class Managers
+{
+    protected string name;
+    protected int salary;
+
+    public Managers(string Name , int Salary)
+    {
+        name = Name;
+        salary = Salary;
+    }
+    public void ReceiveworkingHour(int workingHour)
+    {
+        Console.WriteLine("manager received working hour;"+workingHour);
+    }
+    public  void Deposit()
+    {
+        Console.WriteLine("Manager {0} deposited {1} to each employee",name,salary);
+
+    }
+    public void REceiveWorking(int workingHour) 
+    {
+        Console.WriteLine("Manager{0} receive working hours:{1} ",name ,workingHour);
+    
+    }
+
+
+}
+public class Employee : Managers
+{
+    private int workingHours;
+   
+
+   
+    public Employee(string  employeeName , int employeesalary):
+        base(employeeName , employeesalary)
+    {
+
+        workingHours = 0;
+        
+            
+    }
+   
+
+    public  void Deposit()
+    {
+        Console.WriteLine("Employee {0} received",name , salary);
+
+    }
+  public void SetWorking(int hours)
+    { workingHours = hours;
+
+      base.ReceiveworkingHour(workingHours);
+    
+    
+    
+    }
+
+}
---
+++ Manager/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Manager;
+
+
+
+ Managers manager = new Managers("Ali mohammadi",12000);
+
+ List<Employee> employees = new List<Employee>()
+{
+    new Employee("Ahmad", 560),
+    new Employee("farhad",800),
+    new Employee("farid",1900)
+
+};
+while (true)
+{
+    foreach (Employee employee in employees)
+    {
+        employee.Deposit();
+
+    }
+    Thread.Sleep(4000);
+
+    foreach(Employee employee in employees)
+    {
+
+        employee.SetWorking(8);
+    }
+
+
+}
